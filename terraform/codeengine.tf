41c5ff65a6a0497b74c873a55902163e39e37839
leandro.siqueira <leandro.siqueira@certsys.com.br>
2022-06-14 09:28:47 -0300

    .

--- terraform/codeengine.tf
+++ terraform/codeengine.tf
@@ -4,7 +4,7 @@
 #                                                             #
 ###############################################################
 
-/* resource "ibm_resource_instance" "codeengine" {
+resource "ibm_resource_instance" "codeengine" {
   name              = local.ce_project_name
   service           = "codeengine"
   plan              = "standard"
@@ -33,4 +33,3 @@ resource "null_resource" "codeengine" {
   }
   depends_on = [ibm_resource_instance.codeengine, ibm_cr_namespace.namespace]
 }
-*/
