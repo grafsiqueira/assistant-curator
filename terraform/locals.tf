5525551898433be59dce2e1998a9c1d8073480e2
leandro.siqueira <leandro.siqueira@certsys.com.br>
2022-06-21 17:24:08 -0300

    criando outras functions

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -91,6 +91,84 @@ locals {
       "name" : "insert-logs-cf",
       "code_path" : "../cloud-functions/insert-logs-cf",
       "user_defined_parameters" : jsonencode([])
+    },
+    {
+      "name" : "create-tables-cf-poc",
+      "code_path" : "../cloud-functions/create-tables-cf",
+      "user_defined_parameters" : <<EOF
+        [
+      {
+        "key":"dbConfig",
+        "value": {
+          "connStr": "DATABASE=${ibm_resource_key.db2.credentials["connection.db2.database"]};HOSTNAME=${ibm_resource_key.db2.credentials["connection.db2.hosts.0.hostname"]};PORT=${ibm_resource_key.db2.credentials["connection.db2.hosts.0.port"]};PROTOCL=TCPIP;UID=${ibm_resource_key.db2.credentials["connection.db2.authentication.username"]};PWD=${ibm_resource_key.db2.credentials["connection.db2.authentication.password"]};Security=SSL;",
+          "primaryTableName": "logs",
+          "secondaryTableName": "conversations",
+          "tertiaryTableName": "calls",
+          "quaternaryTableName": "contextVariables",
+          "quinaryTableName": "conversationPath"
+        }
+      },
+      {
+        "key":"assistantConfig",
+        "value": {
+          "apiKey": "${ibm_resource_key.assistant.credentials.apikey}",
+          "version": "2021-06-14",
+          "serviceUrl": "${ibm_resource_key.assistant.credentials.url}",
+          "skillID": "${var.skillID}",
+          "transferNode": ["example1", "example2"],
+          "feedbackNode": ["example1", "example2"],
+          "relevantTopics": ["example1", "example2"],
+          "finalNode": ["example1"]
+        }
+      },
+      {
+        "key":"nluConfig",
+        "value": {
+          "language": "pt",
+          "version": "2021-08-01",
+          "apikey": "${ibm_resource_key.nlu.credentials.apikey}",
+          "serviceUrl": "${ibm_resource_key.nlu.credentials.url}"
+        }
+      },
+      {
+        "key":"cosConfig",
+        "value": {
+          "endpoint": "${local.cos_endpoint}",
+          "apiKeyId": "${var.ibmcloud_api_key}",
+          "serviceInstanceId": "${local.cos_crn}",
+          "bucketName": "${local.bucket_name}"
+        }
+      },
+      {
+        "key":"cloudantConfig",
+        "value": {
+        "apiKey":"${ibm_resource_key.cloudant.credentials.apikey}",
+        "url":"${ibm_resource_key.cloudant.credentials.url}",
+        "dbName":"logs"
+    }
+      }
+    ]
+     EOF
+    },
+    {
+      "name" : "process-logs-cf-poc",
+      "code_path" : "../cloud-functions/process-logs-cf",
+      "user_defined_parameters" : jsonencode([])
+    },
+    {
+      "name" : "process-conversations-cf-poc",
+      "code_path" : "../cloud-functions/process-conversations-cf",
+      "user_defined_parameters" : jsonencode([])
+    },
+    {
+      "name" : "enrichment-cf-poc",
+      "code_path" : "../cloud-functions/enrichment-cf",
+      "user_defined_parameters" : jsonencode([])
+    },
+    {
+      "name" : "insert-logs-cf-poc",
+      "code_path" : "../cloud-functions/insert-logs-cf",
+      "user_defined_parameters" : jsonencode([])
     }
   ]
 }
